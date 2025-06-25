19a2eb31c47deef3df791728a86cd2da5099ba2b
Hugo Pontello <hpontello81@gmail.com>
2025-06-25 12:55:41 -0300

    :bug: fix: Ajusta __init__.py para permitir importações relativas

---
+++ Projeto/back/controllers/__init__.py
@@ -0,0 +1,5 @@
+# controllers/__init__.py
+
+from . import cliente_controller
+from . import equipamento_controller
+from . import reserva_controller
---
+++ Projeto/back/models/__init__.py
@@ -0,0 +1,5 @@
+# models/__init__.py
+
+from . import cliente_model as clienteModel
+from . import equipamento_model as equipamentoModel
+from . import reserva_model as reservaModel
