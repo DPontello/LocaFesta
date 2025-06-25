7fcd6aacf14a1539c76640e5217fccae272c3758
Hugo Pontello <hpontello81@gmail.com>
2025-06-25 11:37:23 -0300

    :ok_hand: style: Padroniza o nome das funções do arquivo app com as regras do Readme

--- Projeto/back/app.py
+++ Projeto/back/app.py
@@ -2,17 +2,17 @@ from flask import Flask
 from flask_cors import CORS
 
 # Importando os Blueprints dos controllers
-from controllers.cliente_controller import cliente_bp
-from controllers.reserva_controller import reserva_bp
-from controllers.equipamento_controller import equipamento_bp
+from controllers.cliente_controller import clienteBp
+from controllers.reserva_controller import reservaBp
+from controllers.equipamento_controller import equipamentoBp
 
 app = Flask(__name__)
 CORS(app)
 
 # Registrar os controllers com prefixo de rota
-app.register_blueprint(cliente_bp, url_prefix='/api')
-app.register_blueprint(reserva_bp, url_prefix='/api')
-app.register_blueprint(equipamento_bp, url_prefix='/api')
+app.register_blueprint(clienteBp, url_prefix='/api')
+app.register_blueprint(reservaBp, url_prefix='/api')
+app.register_blueprint(equipamentoBp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
